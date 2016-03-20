7c8a2853fa2ccb9dd6c0d8cc557bdea9aa28f3ef
Khoi Nguyen <khoi.nguyen.wz@gmail.com>
2016-03-20 22:55:35 +0700

    back up for assignment

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ActiveRecord::Base
 	belongs_to :sender, class_name: "User", foreign_key: "sender_id"
     belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
     paginates_per 5
+    mount_uploader :avatar, AvatarUploader
 end
