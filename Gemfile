fed7d775a957dbc3cec652debf2158ac365f6ba8
Khoi Nguyen <khoi.nguyen.wz@gmail.com>
2016-03-20 10:41:43 +0700

    heroku test

--- Gemfile
+++ Gemfile
@@ -46,3 +46,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'simple_form'
+gem 'omniauth'
+gem 'omniauth-facebook', '1.4.0'
+gem 'carrierwave'
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :friendships
   resources :users
   resources :sessions, only: [:new, :create]
-  delete 'logout' => 'sessions#destroy'
+  get 'logout' => 'sessions#destroy'
   root 'welcome#index'
   resources :messages
   get 'sent_messages' => 'messages#get_sent_message'
